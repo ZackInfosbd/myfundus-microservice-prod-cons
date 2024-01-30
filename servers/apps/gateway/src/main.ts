7d282fa9370f1256cdd48d811d84edfad9faf089
Zakaria <68170594+ZackInfosbd@users.noreply.github.com>
2024-01-30 20:22:54 +0100

    ci: set up docker for auth service and its postgresql db

--- servers/apps/gateway/src/main.ts
+++ servers/apps/gateway/src/main.ts
@@ -5,7 +5,7 @@ import { Logger } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const logger = new Logger();
-  const PORT = process.env.PORT || 4000;
+  const PORT = process.env.PORT || 5000;
 
   await app.listen(`${PORT}`);
   logger.log(`API Gateway listening on port ${PORT} 🚀`);
